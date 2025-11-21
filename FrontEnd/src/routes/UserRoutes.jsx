59a6dd4538f9f6354361eb027a0233e98cb62a25
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-11-21 13:38:04 +0700

    debug

--- FrontEnd/src/routes/UserRoutes.jsx
+++ FrontEnd/src/routes/UserRoutes.jsx
@@ -29,7 +29,7 @@ const UserRoutes = () => {
         <Route path="cart" element={<CartPage />} />
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="checkout-success/:orderId" element={<CheckoutSuccess />} />
-        <Route path="/checkout-failed/:orderId" element={<CheckoutFailed />} />
+        <Route path="checkout-failed/:orderId" element={<CheckoutFailed />} />
         <Route path="order-history" element={<OrderHistory />} />
         <Route path="orders" element={<OrderPage />} />
         <Route path="orders-detail/:id" element={<OrderDetail />} />
